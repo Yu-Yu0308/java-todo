3db8ede1db96b7ac8b39262050f08719144d171d
Yu-Yu0308 <kousentakagi@gmail.com>
2025-10-19 19:19:53 +0900

    ・ファイルへのタスクの追加 ・コマンドライン上でのアクションの選択（間違えて入力した場合の処理も追加） ・定数を別のクラスで宣言

---
+++ Action.java
@@ -0,0 +1,6 @@
+public class Action {
+    public static final String ADD      =   "1";
+    public static final String DELETE   =   "2";
+    public static final String EDIT     =   "3";
+    public static final String SHOW     =   "4";
+}
