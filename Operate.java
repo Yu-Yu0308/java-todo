b1e44e20a4e1d4a8b0fe388735e4775587938e08
Yu-Yu0308 <kousentakagi@gmail.com>
2025-10-22 15:57:24 +0900

    ・削除の機能を追加 ・taskとなっていた部分をtodoに変更
    出来ていないこと
    ・アクションを取り消すこと
    ・アクションをループで実行させること

--- Operate.java
+++ Operate.java
@@ -1,20 +1,60 @@
 import java.util.List;
+import java.util.ArrayList;
 
 
 public class Operate {
 
-    static void add(List<String> lists, String task, String taskStatus){
-        if( taskStatus.equals("0") ){
-            System.out.println("「" + task + " ( 状態： 未完了 ） 」が追加されました。");
-        }else if( taskStatus.equals("1")){
-            System.out.println("「" + task + " ( 状態： 完了 ） 」が追加されました。");
+    static void add(List<String> lists, String todo, String todoStatus){
+
+        if( todoStatus.equals("0") ){
+
+            System.out.println("「" + todo + " ( 状態： 未完了 ） 」が追加されました。");
+
+        }else if( todoStatus.equals("1")){
+
+            System.out.println("「" + todo + " ( 状態： 完了 ） 」が追加されました。");
+
         }
-        lists.add(task + "," + taskStatus);
+
+        lists.add(lists.size() + 1 + "," + todo + "," + todoStatus);
         Todo.overwriting(lists);
+
     }
 
     static void delete(List<String> lists, int num){
-        lists.remove(num-1);        //リストから任意のタスクを削除
+
+        //削除したいTodoのレコードを取得 & 要素ごとに分割
+        String   todoRecode       =   lists.get( num-1 );                                     
+        String[] forSpritRecode   =   todoRecode.split(Todo.COMMA);
+        String   todoNum          =   forSpritRecode[Todo.ELE_NUM_OF_TODO_NUM];
+        String   todo             =   forSpritRecode[Todo.ELE_NUM_OF_TODO_CONTENT];           
+        String   todoStatus       =   forSpritRecode[Todo.ELE_NUM_OF_TODO_STATE];
+
+        List<String> newLists = new ArrayList<>();
+        
+        
+        if(todoStatus.equals(Todo.NOT_COMPLETE_SIGNAL)){
+            todoStatus = Todo.NOT_COMPLETE;
+        }else{
+            todoStatus = Todo.COMPLETE;
+        }
+
+        //リストから任意のTodoを削除
+        System.out.println("「" + todoStatus + "   " + todoNum + "：" + todo + "」を削除しました。");
+        lists.remove(num-1);        
+
+        //Todoの番号振り直し
+        for(int i = 0; i < lists.size(); i++){
+            String   newTodoRecode       =   lists.get( i );                                     
+            String[] forSpritNewRecode   =   newTodoRecode.split(Todo.COMMA);
+            String   newTodo             =   forSpritNewRecode[Todo.ELE_NUM_OF_TODO_CONTENT];           
+            String   todoNewStatus       =   forSpritNewRecode[Todo.ELE_NUM_OF_TODO_STATE];
+
+
+            newLists.add(i + 1 + "," + newTodo + "," + todoNewStatus);
+        }
+
+        Todo.overwriting(newLists);
     }
 
 
